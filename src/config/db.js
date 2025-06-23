79971618d0efd0f35c0bc514e0d70c8c6ec8ad9a
kevinstifel <krstifel@uc.cl>
2025-06-22 22:04:43 -0400

    variables de ambiente y db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres', 
-  host: 'localhost',
-  database: 'evaluacion_pares',
-  password: '123456',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
